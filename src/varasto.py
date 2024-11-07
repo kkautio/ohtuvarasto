a01e15fcb748e528b34506f9b066b458fff24156
kkautio <kkautio@jyu.fi>
2024-11-07 12:12:22 +0200

    Muutettu pylintin määrityksien mukaisesti, ei mene täysin läpi

--- src/varasto.py
+++ src/varasto.py
@@ -42,4 +42,8 @@ class Varasto:
         return maara
 
     def __str__(self):
-        return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
+        return (
+            f"saldo = {self.saldo}, "
+            f"vielä tilaa {self.paljonko_mahtuu()}"
+        )
+    
